4e61e0525c77febecf8f25e2dce7f71ffe682003
Ridwan0126 <muhamadridwan6792@gmail.com>
2024-11-07 14:29:20 +0700

    Commit01/Ridwan/update tampilan-home-tentang-kontak

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,9 @@
+const Blogs = () => {
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+};
+
+export default Blogs;
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,21 @@
+import Blogs from "../components/Blogs";
+
+export default function Blog() {
+  return (
+    <>
+      <main className="grid min-h-full  bg-[#FBFBFB] px-6 py-24 sm:py-32 lg:px-8">
+        {/* Section 4 */}
+        <div className="my-10 mx-5 text-center">
+          <div className="text-center">
+            <h1 className="mt-4 text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-7xl">
+              Halaman Blog
+            </h1>
+          </div>
+        </div>
+        {/* Section 4 */}
+
+        <Blogs />
+      </main>
+    </>
+  );
+}
