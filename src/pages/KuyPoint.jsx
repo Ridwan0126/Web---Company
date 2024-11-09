5df17c08da6ea592da314058ea712ecc9fb7eb13
Ajeng <ajengpramesti37.ap@gmail.com>
2024-11-09 17:31:45 +0800

    Melengkapi Halaman Kuy Point

--- src/pages/KuyPoint.jsx
+++ src/pages/KuyPoint.jsx
@@ -1,27 +1,23 @@
 import KuyPointInfo from "../components/KuyPointInfo";
-import BannerKuyPoint from "../assets/BannerKuyPoint.png"; // image for the KuyPoint banner
-import JoinMovement from "../assets/JoinMovementBanner.png"; // image for the bottom banner
+import BannerKuyPoint from "../assets/BannerKuyPoint.png"; 
+import JoinMovement from "../assets/JoinMovementBanner.png";
 
 export default function KuyPoint() {
   return (
     <>
-<main className="grid min-h-full bg-[#FBFBFB] px-6 py-24 sm:py-32 lg:px-8">
-  {/* KuyPoint Banner */}
-  <div className="place-items-center">
-    <img src={BannerKuyPoint} alt="KuyPoint Banner" />
-  </div>
-  
-  {/* Adjusted spacing for KuyPointInfo */}
-  <div className="my-5 mb-24 mx-5 mt-14"> {/* Increase the top margin here */}
-    <KuyPointInfo />
-  </div>
+      <main className="grid min-h-full bg-[#FBFBFB] px-6 py-24 sm:py-32 lg:px-8">
+        <div className="place-items-center">
+          <img src={BannerKuyPoint} alt="KuyPoint Banner" />
+        </div>
+        
+        <div className="my-5 mb-24 mx-5 mt-14">
+          <KuyPointInfo />
+        </div>
 
-  {/* Bottom banner: Join Movement */}
-  <div className="place-items-center mt-10">
-    <img src={JoinMovement} alt="Join Movement Banner" />
-  </div>
-</main>
-
+        <div className="place-items-center mt-10">
+          <img src={JoinMovement} alt="Join Movement Banner" />
+        </div>
+      </main>
     </>
   );
 }
