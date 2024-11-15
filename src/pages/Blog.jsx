49bec5bc0097018b305a2f249d1267ddaa4d6975
Ajeng <ajengpramesti37.ap@gmail.com>
2024-11-15 21:10:38 +0800

    Update Blog.jsx

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,42 +1,30 @@
-// Blogs.jsx
-import BannerSampah from "../assets/Banner02.png"; // Import the banner image
+import BannerSampah from "../assets/Banner02.png"; 
+import BlogImage1 from "../assets/blog1.png"; 
+import BlogImage2 from "../assets/blog2.png";
+import BlogImage3 from "../assets/blog3.png";
+import Blogs from "../components/Blogs"; 
 
-export default function Blogs() {
+export default function Blog() {
+
+  const images = [BlogImage1, BlogImage2, BlogImage3];
+
   const blogs = Array.from({ length: 12 }).map((_, idx) => ({
     id: idx + 1,
-    title: `Blog Post ${idx + 1}`,
-    description: `This is the description of blog post ${idx + 1}.`,
-    image: "https://via.placeholder.com/150", 
+    title: `Kuy Point: Cara Kumpul & Tukar Hadiah`,
+    description: `Daur ulang dan dapatkan hadiah! Simak cara mudah mengumpulkan Kuy Point dan menukarkannya dengan reward menarik.`,
+    image: images[idx % 3], 
     link: "#", 
   }));
 
   return (
     <main className="bg-[#FBFBFB] px-6 py-24 sm:py-32 lg:px-8">
       {/* Banner */}
-      <div className="w-full mb-8">
+      <div className="w-full mb-16"> 
         <img src={BannerSampah} alt="banner sampah" className="w-full object-cover" />
       </div>
 
-      {/* Blogs Post*/}
-      <div className="max-w-7xl mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogs.map((post) => (
-            <div key={post.id} className="bg-[#FBFBFB] overflow-hidden">
-              <img src={post.image} alt={post.title} className="w-full h-48 object-cover" />
-              <div className="p-6">
-                <h2 className="text-2xl font-semibold mb-2 text-[#000000]">{post.title}</h2>
-                <p className="text-gray-600 mb-4">{post.description}</p>
-                <a
-                  href={post.link}
-                  className="text-white bg-[#55B3A4] py-2 px-4 rounded-md inline-block"
-                >
-                  Read more &rarr;
-                </a>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      {/* Blog Posts */}
+      <Blogs blogs={blogs} />
     </main>
   );
 }
