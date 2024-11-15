79814453474a255afe9294834598e6dece2895d6
Nayya05 <nayyatm2911@gmail.com>
2024-11-15 12:47:06 +0700

    Revisi YukBuang

--- src/pages/YukBuang.jsx
+++ src/pages/YukBuang.jsx
@@ -14,6 +14,7 @@ const YukBuang = () => {
   const handleStepClick = (step) => {
     setSelectedStep(prevStep => (prevStep === step ? null : step));
   };
+  
 
   return (
     <div className="flex flex-col items-center">
